cb518cb4d1f77958815ade25fc6cef75b8409d7f
Luiza <luiza.mustafaeva1997@gmail.com>
2022-09-01 18:20:30 +0500

    Create the List of Landmarks and make the List Dynamic

---
+++ LandmarksApp/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  LandmarksApp
+//
+//  Created by Louise on 01/09/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
