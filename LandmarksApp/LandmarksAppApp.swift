bfba7650d309193d18c6b9e9f437e20b3ed0843b
Luiza <luiza.mustafaeva1997@gmail.com>
2022-09-01 13:54:22 +0500

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Louise on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
