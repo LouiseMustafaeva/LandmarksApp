7df01076150abfa54b608fcc817ebe7f510973ec
Luiza <luiza.mustafaeva1997@gmail.com>
2022-09-02 16:12:42 +0500

    Adopt the Model Object in View

--- LandmarksApp/Views/LandmarkList.swift
+++ LandmarksApp/Views/LandmarkList.swift
@@ -8,20 +8,18 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    
+    @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationView {
-            
             List {
-                
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
