a522eef7be4ebf0d87c64eb958085e80b834af08
Luiza <luiza.mustafaeva1997@gmail.com>
2022-09-03 16:26:18 +0500

    Create a Category List

---
+++ LandmarksApp/Views/Categories/CategoryHome.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryHome.swift
+//  LandmarksApp
+//
+//  Created by Louise on 03/09/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
