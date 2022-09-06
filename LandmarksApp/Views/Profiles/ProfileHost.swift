27e0d342dee4a99075f2bc8d5dcd7bf7fe7fb2b4
Luiza <luiza.mustafaeva1997@gmail.com>
2022-09-06 16:16:41 +0500

    Display a User Profile

---
+++ LandmarksApp/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  LandmarksApp
+//
+//  Created by Louise on 06/09/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
